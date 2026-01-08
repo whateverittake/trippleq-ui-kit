9f90f8d52a6f98284434199843ff218dc44999e6
tth8585 <sinlotaradin@gmail.com>
2026-01-08 18:04:18 +0700

    update

--- Runtime/UI/ITrippleQPopupView.cs
+++ Runtime/UI/ITrippleQPopupView.cs
@@ -10,7 +10,6 @@ namespace TrippleQ.UiKit
         // lifecycle UI
         void Show();
         void Hide();
-        void SetInteractable(bool interactable);
 
         // content
         void SetTitle(string title);
@@ -20,8 +19,5 @@ namespace TrippleQ.UiKit
         void SetPrimary(string label, Action onClick);
         void SetSecondary(string label, Action onClick);
         void SetClose(Action onClick);
-
-        // optional: loading / busy
-        void SetLoading(bool isLoading);
     }
 }
